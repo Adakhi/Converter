a27ed827698da535688a2e181b4f3dc915a080fa
Adakhi <stanislavadakhi@gmail.com>
2021-08-27 01:34:38 +0400

    First release of Online converter

---
+++ myFirst.js
@@ -0,0 +1,20 @@
+function temperatureConverter(valNum) {
+  valNum = parseFloat(valNum);
+  document.getElementById("demo").innerHTML= "C" + (valNum-32)*5/9 + "&deg";
+}
+
+function milesConverter(valNum) {
+  valNum = parseFloat(valNum);
+  document.getElementById("mile").innerHTML= valNum * 1.609344 + " " + "Km";
+}
+
+function moneyConverter(valNum) {
+  valNum = parseFloat(valNum);
+  document.getElementById("money").innerHTML= valNum * 3.1288 + " " + "GEL";
+}
+
+
+  
+
+
+
