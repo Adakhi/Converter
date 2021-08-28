edc5f1229d696557d93d55f962cf01e981e82dbc
Adakhi <stanislavadakhi@gmail.com>
2021-08-28 21:06:43 +0400

    change input type

--- myFirst.js
+++ myFirst.js
@@ -1,11 +1,15 @@
-function temperatureConverter(valNum) {
+function showAmount(valNum,newAmount) {
   valNum = parseFloat(valNum);
+  newAmount = parseFloat(valNum);
   document.getElementById("demo").innerHTML= (valNum-32)*5/9 + "C" + "&deg";
+  document.getElementById("amount").innerHTML = newAmount;
 }
 
-function milesConverter(valNum) {
+function showAmount1(valNum,newAmount) {
   valNum = parseFloat(valNum);
+  newAmount = parseFloat(valNum);
   document.getElementById("mile").innerHTML= valNum * 1.609344 + " " + "Km";
+  document.getElementById("amount1").innerHTML = newAmount;
 }
 
 function moneyConverter(valNum) {
